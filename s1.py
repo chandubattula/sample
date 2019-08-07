b0791362f5c6490e421e5049f460d5986f287fbf
chandubattula <53875277+chandubattula@users.noreply.github.com>
2019-08-08 00:09:51 +0530

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,7 +1,7 @@
 num=int(input())
 if num>0:
     print("positive")
-    elif n==0:
-        print("zero")
-        else:
-            print("negative")
+elif num==0:
+   print("zero")
+else:
+   print("negative")
